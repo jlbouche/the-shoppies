b7ba84461c1ff1d2681f820fd230fd522c72f917
JL <jennalbouche@gmail.com>
2021-04-27 14:06:48 -0700

    structural setup for form component, app page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
+import Form from '../components/Form/Form';
 
 import './App.css';
 
 function App() {
   const [movieData, setMovieData] = useState("");
   const [movieTitle, setMovieTitle] = useState("");
+  const [movieNomination, setMovieNomination] = useState([]);
 
+
+  
   return (
     <div className="App">
-      
+      <Form handleChange={handleChange}/>
     </div>
   );
 }
