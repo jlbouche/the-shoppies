bc9b5fa411c55ca3c303ee9a35e6b8856a1db62e
JL <jennalbouche@gmail.com>
2021-04-28 10:31:00 -0700

    remove form component, add to app page, add fetch code, update display results

--- src/components/APIResults/APIResults.js
+++ src/components/APIResults/APIResults.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {List} from 'semantic-ui-react';
 
-export default function APIResults({ movie }) {
+function APIResults({ movie }) {
+
     return (
         <List>
-            <List.Item icon='video'>{movie.Title}{movie.Year}</List.Item>
+            <List.Item icon='video'>{movie.Title} ({movie.Year})</List.Item>
         </List>
     );
 }
+
+export default APIResults;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from "react";
-import SearchForm from '../components/SearchForm/SearchForm';
 import APIResults from '../components/APIResults/APIResults';
+import {Form} from "semantic-ui-react";
 
 import './App.css';
 
 function App() {
-  const [movieData, setMovieData] = useState("");
+  const [movieData, setMovieData] = useState([]);
   const [movieTitle, setMovieTitle] = useState("");
   const [movieNomination, setMovieNomination] = useState([]);
 
+  const handleChange = (e) => {
+    const title = e.target.value;
+    console.log(title)
+    setMovieTitle(title);
+  };
 
+  useEffect(() => {
+    const movieUrl = `https://www.omdbapi.com/?t=${movieTitle}&apikey=98e3fb1f`;
+    fetch(movieUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovieData(data);
+      });
+  }, [movieTitle]);
   
   return (
     <div className="App">
-      <SearchForm />
-      {<APIResults />
+      <Form>
+            <Form.Field>
+                <label>Movie Title</label>
+                <input onChange={handleChange}/>
+            </Form.Field>
+        </Form>
+      {movieTitle ? <APIResults movie={movieData}/> : null}
     </div>
   );
 }
