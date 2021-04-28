7319851190e19728909ff90a98a6848d56ff8fa4
JL <jennalbouche@gmail.com>
2021-04-28 15:44:47 -0700

    add error catch for data input

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,42 +1,48 @@
 import React, { useEffect, useState } from "react";
 import APIResults from '../components/APIResults/APIResults';
-import {Form} from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 import './App.css';
 
 function App() {
   const [movieData, setMovieData] = useState("");
-  const [movieTitle, setMovieTitle] = useState("");
+  const [searchText, setSearchText] = useState("");
   const [movieNomination, setMovieNomination] = useState([]);
 
   const handleChange = (e) => {
-    let title = e.target.value;
-    console.log(title)
-    setMovieTitle(title);
-    console.log(movieTitle);
+    let search = e.target.value;
+    console.log(search)
+    setSearchText(search);
+    console.log(searchText);
   }
 
+
   useEffect(() => {
-    if (movieTitle.length >= 4){
-      const movieUrl = `https://www.omdbapi.com/?s=${movieTitle}&apikey=98e3fb1f`;
+    if ((searchText.length <= 10) && (searchText.length >=3)){
+      const movieUrl = `https://www.omdbapi.com/?s=${searchText}&apikey=98e3fb1f`;
+      console.log(movieUrl);
       fetch(movieUrl)
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
-          setMovieData(data)
+          if (data.Response === "True") {
+            setMovieData(data)
+          }
+          else {
+            setMovieData(null);
+          }
         });
     }
-  }, [movieTitle]);
+  }, [searchText]);
   
   return (
     <div className="App">
       <Form>
             <Form.Field>
-                <label>Movie Title</label>
+                <label>Search Movies</label>
                 <input onChange={handleChange}/>
             </Form.Field>
         </Form>
-      {movieData ? <APIResults movie={movieData}/> : null}
+      {movieData ? <APIResults movie={movieData} /> : null}
     </div>
   );
 }
