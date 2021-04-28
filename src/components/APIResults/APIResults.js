9d41589e21c82ad25b2ce86929946ce2397ce36c
JL <jennalbouche@gmail.com>
2021-04-28 13:48:27 -0700

    display first 3 results, require min of 3 char to run fetch

--- src/components/APIResults/APIResults.js
+++ src/components/APIResults/APIResults.js
@@ -6,6 +6,8 @@ function APIResults({ movie }) {
     return (
         <List>
             <List.Item icon='video'>{movie.Search[0].Title} ({movie.Search[0].Year})</List.Item>
+            <List.Item icon='video'>{movie.Search[1].Title} ({movie.Search[1].Year})</List.Item>
+            <List.Item icon='video'>{movie.Search[2].Title} ({movie.Search[2].Year})</List.Item>
         </List>
     );
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -12,20 +12,20 @@ function App() {
   const handleChange = (e) => {
     let title = e.target.value;
     console.log(title)
-    if (title.length >= 3){
-      setMovieTitle(title);
-      console.log(movieTitle);
-    }
-  };
+    setMovieTitle(title);
+    console.log(movieTitle);
+  }
 
   useEffect(() => {
-    const movieUrl = `https://www.omdbapi.com/?s=${movieTitle}&apikey=98e3fb1f`;
-    fetch(movieUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMovieData(data);
-      });
+    if (movieTitle.length >= 3){
+      const movieUrl = `https://www.omdbapi.com/?s=${movieTitle}&apikey=98e3fb1f`;
+      fetch(movieUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setMovieData(data);
+        });
+    }
   }, [movieTitle]);
   
   return (
@@ -36,7 +36,7 @@ function App() {
                 <input onChange={handleChange}/>
             </Form.Field>
         </Form>
-      {movieTitle ? <APIResults movie={movieData}/> : null}
+      {movieData ? <APIResults movie={movieData}/> : null}
     </div>
   );
 }
