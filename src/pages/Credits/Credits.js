78589cf8354f920569bb5a26cf070deeeba7037f
JL <jennalbouche@gmail.com>
2021-05-06 10:45:24 -0700

    remove about page, add modal for homescreen to display about info, update menu to remove about route, skeleton credits page

--- src/pages/Credits/Credits.js
+++ src/pages/Credits/Credits.js
@@ -13,7 +13,20 @@ function Credits(){
             <Menu.Item as={Link} to='/vote'><Icon name='search plus'/>Search/Vote</Menu.Item>
             <Menu.Item as={Link} to='/credits' position='right'><Icon name='hand point right'/>Credits</Menu.Item>
         </Menu>
-        <Header>I am the credits page!</Header>
+        <Header as='h1' color='red'>Credits</Header>
+        <Grid columns={3} divided>
+            <Grid.Row>
+                <Grid.Column>
+
+                </Grid.Column>
+                <Grid.Column>
+
+                </Grid.Column>
+                <Grid.Column>
+
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
         </>
     )
 }
